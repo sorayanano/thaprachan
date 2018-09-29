b1890c6fcbd94da0bb1e8e77442b98c0929aba7c
sorayanano <43529261+sorayanano@users.noreply.github.com>
2018-09-29 18:44:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/user/thaprachan/thaprachan_member.java
@@ -0,0 +1,13 @@
+package com.example.user.thaprachan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class thaprachan_member extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thaprachan_member);
+    }
+}
---
+++ app/src/main/java/com/example/user/thaprachan/thaprachan_menu4.java
@@ -0,0 +1,13 @@
+package com.example.user.thaprachan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class thaprachan_menu4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thaprachan_menu4);
+    }
+}
